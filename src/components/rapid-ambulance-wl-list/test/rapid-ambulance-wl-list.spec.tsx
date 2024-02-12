f9ed55a1f1fb666592d5c473ac0180085a28ab62
MartinRapcan <rapcan.rmartin@gmail.com>
2024-02-12 11:53:18 +0100

    feat: unit tests

--- src/components/rapid-ambulance-wl-list/test/rapid-ambulance-wl-list.spec.tsx
+++ src/components/rapid-ambulance-wl-list/test/rapid-ambulance-wl-list.spec.tsx
@@ -7,12 +7,10 @@ describe('rapid-ambulance-wl-list', () => {
       components: [RapidAmbulanceWlList],
       html: `<rapid-ambulance-wl-list></rapid-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <rapid-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </rapid-ambulance-wl-list>
-    `);
+    const wlList = page.rootInstance as RapidAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length;
+
+    const items = page.root.shadowRoot.querySelectorAll('md-list-item');
+    expect(items.length).toEqual(expectedPatients);
   });
 });
